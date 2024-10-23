d673b83e8acb4e791e46134024ae9bd33007a991
qudo-code <>
2024-10-23 12:46:13 -0700

    hyprland hardware cursor

--- nix/flake.nix
+++ nix/flake.nix
@@ -7,14 +7,16 @@
         nixpkgs.url = "github:nixos/nixpkgs";
         zen-browser.url = "github:MarceColl/zen-browser-flake";
         hyprland.url = "github:hyprwm/Hyprland";
-        machine-flake.url = "./machine.nix";
+        machine-flake = {
+            url = "path:./nix/machine/flake.nix";
+        };
     };
 
     # use them to output nixos config
     outputs = { ... }@ inputs:
         let
             # machineModule = import ./machine.nix;
-            machine = machine-flake.outputs {
+            machine = my_thing = pkgs.callPackage ./nix/machine/flake.nix {
                 inherit inputs;
             };
 

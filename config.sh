96814404040bdad3648a2a949fe192f6a0bcb4de
qudo-code <qudo@matr.world>
2025-04-12 10:48:19 -0700

    sync

--- config.sh
+++ config.sh
@@ -24,6 +24,17 @@ install_macos() {
 
     brew install --cask flameshot
     brew install --cask sf-symbols
+
+    # nvm
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
+    export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+    # bun
+    curl -fsSL https://bun.sh/install | bash
+
+    # pnpm
+    curl -fsSL https://get.pnpm.io/install.sh | sh -
 }
 
 sync_macos() {

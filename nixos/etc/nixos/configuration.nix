3e45b7ce48513982ae909230c8a8d7a90809b074
qudo-code <qudo@matr.world>
2025-04-29 10:04:38 -0700

    sync

--- nixos/etc/nixos/configuration.nix
+++ nixos/etc/nixos/configuration.nix
@@ -46,8 +46,6 @@
     git
     bun
     unzip
-    prisma
-    openssl
     pnpm
     bun
     wofi
@@ -67,6 +65,26 @@
     hyprshot
     hyprpaper
   ];
+
+  pkgs.mkShell {
+    buildInputs = with pkgs; [
+        nodejs_20
+        nodePackages.yarn
+        nodePackages.node-gyp
+        vips
+        python3
+        prisma-engines
+        openssl
+    ];
+
+    shellHook = ''
+        export PRISMA_SCHEMA_ENGINE_BINARY="${pkgs.prisma-engines}/bin/schema-engine"
+        export PRISMA_QUERY_ENGINE_BINARY="${pkgs.prisma-engines}/bin/query-engine"
+        export PRISMA_QUERY_ENGINE_LIBRARY="${pkgs.prisma-engines}/lib/libquery_engine.node"
+        export PRISMA_FMT_BINARY="${pkgs.prisma-engines}/bin/prisma-fmt"
+        export PATH="$PWD/node_modules/.bin/:$PATH"
+    '';
+  }
 
   # Enable other programs
   programs.hyprland.enable = true;
